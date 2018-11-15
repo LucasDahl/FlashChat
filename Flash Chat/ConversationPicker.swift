e777e4bcdfeddb1085690597388e93181301376c
Lucas Dahl <LucasDahl425@yahoo.com>
2018-11-15 07:16:59 -0800

    Added the basic UI and class for conversationPicker.

---
+++ Flash Chat/ConversationPicker.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationPicker.swift
+//  Flash Chat
+//
+//  Created by Lucas Dahl on 11/15/18.
+//  Copyright © 2018 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ConversationPicker: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
